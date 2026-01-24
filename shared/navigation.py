546508058c558b23e50e0f8c06dcb6427c585853
jasonniebauer <lookwhatjasondid@gmail.com>
2026-01-24 11:00:55 -0600

    Clean up

--- shared/navigation.py
+++ shared/navigation.py
@@ -85,8 +85,4 @@ def render_navigation():
             '<h6>Made by <a href="https://jasonniebauer.com" style="text-decoration:underline;">Jason Niebauer</a></h6>',
             unsafe_allow_html=True,
         )
-        # st.markdown(
-        #     '<div style="margin-top: 0.75em;"><a href="https://www.buymeacoffee.com/andfanilo" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a></div>',
-        #     unsafe_allow_html=True,
-        # )
         
